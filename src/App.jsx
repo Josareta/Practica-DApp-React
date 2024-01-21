7c5c1977839d736d3f59f3d4598ce49d5465e503
Josareta <josareta@gmail.com>
2024-01-21 11:27:24 +0100

    Añadir header

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 import { useState } from 'react'
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+function App() { 
   return (
     <>
-      <div className='bg-red-200'>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>  
+      <Header/>
+      <main>Este es el contenido principal</main>
+      <Footer/>
+
     </>
+
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return <header className="flex px-8 py-4 border-b-2 justify-between items-center bg-white" >
+        {/*Logo para móvil*/}
+        <img src="/icono.png" className="sm:hidden" alt="BlockMaker logo" width={45}/>
+        {/*Logo para desktop*/}
+        <img src="/logo-blockmaker.png" className="hidden sm:flex" alt="BlockMaker logo" width={200}/>
+        
+        <button className="bg-gray-200 rounded-xl">Conectar Wallet</button>
+    </header>
+
+}

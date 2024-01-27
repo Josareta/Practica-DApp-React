9cac67b3c436276306ce7a50d85fc65d1626e6c1
Josareta <josareta@gmail.com>
2024-01-27 17:53:08 +0100

    Crear wagmi en configuración

---
+++ src/config/wagmi.js
@@ -0,0 +1,24 @@
+
+import { createConfig } from "wagmi";
+import { getDefaultConfig } from "connectkit";
+import { sepolia} from "wagmi/chains";
+import { mainnet } from '@wagmi/core/chains';
+//Para comprobar que la variable de netorno, se ha cargado correctamente
+console.log(import.meta.env.VITE_ALCHEMY_ID)
+console.log(import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID)
+export const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: import.meta.env.VITE_ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+
+    // Required
+    appName: "BlockMaker ERG-20 App",
+    chains: [sepolia, mainnet]
+  
+    // Optional
+    //appDescription: "Your App Description",
+    //appUrl: "https://family.co", // your app's url
+    //appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);

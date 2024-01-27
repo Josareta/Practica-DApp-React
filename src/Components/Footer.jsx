52979d303457ae2cabc2b8c68c39adb890e96106
Josareta <josareta@gmail.com>
2024-01-27 17:50:47 +0100

    Modificar Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,6 @@
 export default function Footer(){
-    return <footer>Esto es el footer</footer>
-}
+    return <footer className="text-xs py-4 px-3 sm: p-8 sm: tex-base  bg-white border-t shadow-md flex justify-center">
+         {/* Símbolo de copyright se crea con &copy;  */ }
+        <p>Derechos de autor &copy; {new Date().getFullYear()} BlockMaker Academy </p>
+    </footer>
+} 

a55fbc682c5ad88773dcfec626d4db9a7a83188d
Josareta <josareta@gmail.com>
2024-01-28 18:47:19 +0100

    Añadir componete Owner

---
+++ src/Components/Owner.jsx
@@ -0,0 +1,20 @@
+import { useContractRead } from 'wagmi'
+import { blockmakerTokenABI } from "../contracts/ABIs"
+import { Title } from './ui'
+//import { formatEther } from 'viem/utils'
+
+export default function Owner() {
+    const { data } = useContractRead({
+        abi: blockmakerTokenABI,
+        address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+        functionName: 'owner'
+    })
+    
+    return (
+        <div className='bg-white p-4 border shadow rounded-md'>
+            <Title>Owner</Title>
+            <p className='bg-gray-100 text-zinc-600 p-2 rounded-md text-s'>{data}</p>
+                   
+        </div>
+    )
+}

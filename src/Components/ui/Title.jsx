9877d48eec1cd0736d4d5bcb37212c6bf1966bad
Josareta <josareta@gmail.com>
2024-01-28 18:43:02 +0100

    Añadir componente Title

---
+++ src/Components/ui/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title({children}){
+    return (
+        <div>
+            <h3 className="font-bold text-lg mb-2 w-fit text-slate-700">{children}</h3>
+        </div>
+
+    )
+}

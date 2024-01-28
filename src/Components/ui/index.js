a8fc65d598b9d086f3fbbfff4537d8b50890cb7d
Josareta <josareta@gmail.com>
2024-01-28 18:43:58 +0100

    Actualizar índice componentes UI

--- src/Components/ui/index.js
+++ src/Components/ui/index.js
@@ -2,3 +2,6 @@
 export {default as LoadingSpinner} from "./LoadingSpinner";
 export {default as Button} from "./Button";
 export {default as ErrorInfo} from "./ErrorInfo";
+export {default as TextInput} from "./TextImput";
+export {default as Title} from "./Title";
+

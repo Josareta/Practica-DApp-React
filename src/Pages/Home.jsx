410f8b74d0bd7f8296cca816b2c79ffb8885bb24
Josareta <josareta@gmail.com>
2024-01-28 18:51:39 +0100

    Añadir Address conctract en .env.example

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,14 @@
-import { useAccount } from "wagmi"
-//import LoadingSpinner from "../Components/ui/LoadingSpinner"
-//import Button from "../Components/ui/Button"
-import { Button, ErrorInfo } from "../Components/ui"
+
+
+import { TokenBalance, Owner } from "../Components"
 
 export default function Home(){
-    const { address, isConnecting, isDisconnected } = useAccount()
-    if (isConnecting) return <div>Connecting...</div>
-    if (isDisconnected) return <div>Disconnected</div>
-    
     return (
-        <div>
-            <h1>Home</h1>
-            <div>Connected Wallet: {address}</div>    
-            <Button isLoading={true} >Mi componente botón</Button>
-            <ErrorInfo message="Este es un componente para mostrar mensajes de error"/>
+        <div className="flex flex-col place-items-center">
+            <h1>Home</h1>   
+            <TokenBalance/>
+            <Owner/>
+            
         </div>
     )
 }

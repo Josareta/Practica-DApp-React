64b59f4f84c7a6c9fd07567c7ce6245c6eb67cf3
Josareta <josareta@gmail.com>
2024-01-30 16:03:29 +0100

    Añadir al home: TokenInfo y TransferTokenForm

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,15 @@
 
 
-import { TokenBalance, Owner } from "../Components"
+import { TokenBalance, Owner, TokenInfo,TransferTokensForm } from "../Components"
 
 export default function Home(){
     return (
         <div className="flex flex-col place-items-center">
-            <h1>Home</h1>   
+            <h1 className="text-4xl py-10">BlockMaker ERC20 Token</h1>   
             <TokenBalance/>
             <Owner/>
+            <TokenInfo/>
+            <TransferTokensForm/>
             
         </div>
     )

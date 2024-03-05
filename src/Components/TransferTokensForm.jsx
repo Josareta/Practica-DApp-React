52d67e87666c2d7332ddb0aa7722da305fcd069c
Josareta <josareta@gmail.com>
2024-03-05 10:06:35 +0100

    Crear repositorio para generar enlace

--- src/Components/TransferTokensForm.jsx
+++ src/Components/TransferTokensForm.jsx
@@ -1,9 +1,28 @@
 import {Button,TextInput, Title } from './ui'
 import { useState } from 'react'
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction} from 'wagmi'
+import { blockmakerTokenABI } from '../contracts/ABIs'
 
 export default function TransferTokensForm (){
     const[to,setTo]= useState("")
     const[amount,setAmount]= useState("")
+
+    const { config } = usePrepareContractWrite({
+        address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+        abi: blockmakerTokenABI,
+        functionName: 'transfer',
+        //BigInt representa valores numéricos muy grandes, de una forma sencilla
+        args: [to, BigInt (amount * 10 ** 18)]
+      })
+
+    const { data: writeData, write } = useContractWrite(config)
+
+    const { isLoading: isTransactionLoading,
+        isSuccess: isTransactionSucess,
+        isError: isTrransactionError
+    } = useWaitForTransaction({
+        hash: writeData?.hash
+    })
     
     const handlerToInputChange = (event) => {
         setTo(event.target.value)
@@ -19,8 +38,9 @@ export default function TransferTokensForm (){
             <Title>TransferFrom</Title>
             <form className='grid gap-4'>
                 <TextInput type="Text"  placeholder="To" onChange={handlerToInputChange} />
-                <TextInput type="number"  placeholder="To" onChange={handlerToInputChange} />
-                <Button>Transfer </Button>
+                <TextInput type="number"  placeholder="Amount" onChange={handlerToInputChange} />
+                <Button disabled={!write || isTransactionLoading} onClick={() => write?.()} isLoading={isTransactionLoading} >{isTransactionLoading? "Transfiriendo BM Tokens..." : " Transferir BM Tokens"} </Button>
+
             </form>
             
         </section>
